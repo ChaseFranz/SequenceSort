84971a43bfa59c4fc579fb0dcac0ebcc54cdb5f7
Chase Franz <chase@chasefranz.com>
2020-07-24 02:34:54 -0500

    Including project. Only Insertion sort is 'finished'

---
+++ SequenceSort/IntArrayExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SequenceSort
+{
+    public static class IntArrayExtensions
+    {
+        /// <summary>
+        /// Sort a sequence of integers with Insertion sort.
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <param name="sortInDescendingOrder"></param>
+        public static void InsertionSort(this int[] sequence, bool sortInDescendingOrder)
+        {
+            Func<int, int, bool> CompareSubArrayItemToKey;
+
+            // Calling this function in iterations allows for optional descending or ascending sorts.
+            if (sortInDescendingOrder)
+            {
+                CompareSubArrayItemToKey = (subArrayItem, key) => subArrayItem < key;
+            }
+            else
+            {
+                CompareSubArrayItemToKey = (subArrayItem, key) => subArrayItem > key;
+            }
+
+            // Insertion Sort Algorithm
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                int key = sequence[i];
+                int subArrayIndex = i - 1;
+
+                while (subArrayIndex > -1 && CompareSubArrayItemToKey(sequence[subArrayIndex],key))
+                {
+                    sequence[subArrayIndex + 1] = sequence[subArrayIndex];
+                    subArrayIndex--;
+                }
+                sequence[subArrayIndex + 1] = key;
+            }
+        }
+
+        /// <summary>
+        /// Puprose is to populate all array indexes with random values. 
+        /// values are within the set [0,n) where n is the array length.
+        /// </summary>
+        /// <param name="sequence"></param>
+        public static void PopulateWithRandomValues(this int[] sequence)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                sequence[i] = rand.Next(0, sequence.Length);
+            }
+        }
+    }
+}
---
+++ SequenceSort/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SequenceSort
+{
+    public class Program
+    {
+        private static int[] sequence;
+        private static int userInput;
+
+        static void Main(string[] args)
+        {
+            sequence = new int[5000];
+            bool isActive = true;
+            Stopwatch stopWatch = new Stopwatch();
+
+            Console.WriteLine("SequenceSort: A project by Chase Franz\n");
+
+            do
+            {
+                AskForUserInput();
+                bool validInput = CollectAndValidateUserInput();
+                
+                // TODO: Use Enum instead of 'magic numbers'
+                if (validInput)
+                {
+                    try
+                    {
+                        switch (userInput)
+                        {
+                            case -1:
+                                // User wants to end loop
+                                isActive = false;
+                                break;
+
+                            case 1:
+                                sequence.PopulateWithRandomValues();
+                                stopWatch.Reset();
+                                stopWatch.Start();
+
+                                // Execute Sort
+                                sequence.InsertionSort(false);
+
+                                stopWatch.Stop();
+                                LogSortDetails(sequence, stopWatch.Elapsed, "Insertion Sort ASC");
+                                break;
+
+                            case 2:
+                                sequence.PopulateWithRandomValues();
+                                stopWatch.Reset();
+                                stopWatch.Start();
+
+                                // Execute Sort
+                                sequence.InsertionSort(true);
+
+                                stopWatch.Stop();
+                                LogSortDetails(sequence, stopWatch.Elapsed, "Insertion Sort DESC");
+                                break;
+
+                            // TODO: Implement merge sort.
+
+                            default:
+                                throw new ArgumentException("Unhandled user input");
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        LogException(ex);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+            while (isActive);
+            Console.WriteLine("Closing Project...");
+        }
+
+        public static bool CollectAndValidateUserInput()
+        {
+            return int.TryParse(Console.ReadLine(), out userInput) && 1 <= userInput && userInput <= 2 || userInput == -1;
+        }
+
+        public static void AskForUserInput()
+        {
+            Console.WriteLine("Please select an option from the list below");
+            Console.WriteLine("\tInsertion Sort Ascending:  1");
+            Console.WriteLine("\tInsertion Sort Descending: 2");
+            Console.WriteLine("\tClose Project:\t-1");
+        }
+
+        public static void LogException(Exception ex)
+        {
+            //Console.Clear();
+            Console.WriteLine(ex);
+        }
+
+        public static void LogSortDetails(int[] sequence, TimeSpan sortTimeSpan, string sortName)
+        {
+            Console.Write("\nOrdered Sequence = { ");
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                Console.Write(sequence[i]);
+                if (i < sequence.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine(" }\n");
+
+            Console.WriteLine($"SORT DETAILS");
+            Console.WriteLine($"Sorting Algorithm: {sortName}");
+            Console.WriteLine($"Execution Time: {sortTimeSpan}");
+            Console.WriteLine("\n\n--------------------------------------------------------------------------------------------------\n");
+        }
+    }
+}
